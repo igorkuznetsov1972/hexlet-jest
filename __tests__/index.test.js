2010f316e421774f3da3c235a18701e356d5ae20
Igor <ikk@mail.ru>
2020-06-10 21:05:15 +0300

    remove babel yay!!!!

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,4 +1,6 @@
-import reverse from '../src/index.js';
+// eslint-disable-next-line import/extensions
+import { test, expect } from '@jest/globals';
+import reverse from '../src/index';
 
 test('reverse', () => {
   expect(reverse('hello')).toEqual('olleh');
